5ac2c1add65e363fbfa6189788a822be7c206fb9
Anonymous <krishnabag751@gmail.com>
2024-10-14 16:06:35 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center">
+        <h1 className="text-3xl font-bold underline text-blue-500">
+          Hello, Tailwind CSS with React and TypeScript!
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
