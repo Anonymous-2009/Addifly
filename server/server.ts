bd1d82efe3edcc2bc02b74a59a200dee92ea45d5
Anonymous <krishnabag751@gmail.com>
2024-10-17 12:09:55 +0530

    five cluster later notification and connection

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,39 @@
 import app from './src/app';
 import 'dotenv/config';
+import process from 'node:process';
+import cluster from 'node:cluster';
+import { availableParallelism } from 'node:os';
 
 const PORT = process.env.PORT || 3000;
+// const cpus = availableParallelism();
+const cpus = 5; // only 5 cpus for dev and use lots no of cpu
 
 const startServer = async () => {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+  if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+    console.log(`Setting up ${cpus} workers...`);
+
+    // Fork workers
+    for (let i = 0; i < cpus; i++) {
+      cluster.fork();
+    }
+
+    // Handle worker exits and restart them
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(
+        `Worker ${worker.process.pid} died. Signal: ${signal}. Code: ${code}`,
+      );
+      console.log('Starting a new worker...');
+      cluster.fork();
+    });
+  } else {
+    // Workers can share any TCP connection
+    app.listen(PORT, () => {
+      console.log(
+        `Worker ${process.pid} started and listening on http://localhost:${PORT}`,
+      );
+    });
+  }
 };
 
 startServer();
